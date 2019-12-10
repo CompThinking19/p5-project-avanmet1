2b0e25b39dcbebc2b8af222429150316e79abc44
avanmet1 <55400228+avanmet1@users.noreply.github.com>
2019-12-10 16:58:16 -0500

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -1,4 +1,3 @@
-
 var txt;
 var lines;
 var poem
@@ -10,17 +9,20 @@ function preload(){
 function setup() {
   createCanvas(1200, 800);
 
+  txt = join(lines, '\n');
   y = height;
 }
 
 function draw() {
+  if (mouseIsPressed) {
+  var poem = random(lines) + '\n' + random(lines)
+  random(lines);
   background(0);
-
   fill(255);
   textSize(32);
   textAlign(CENTER);
-  text(poem, 0, y, width, height*100);
-  y-=10;
-  //define poem within mouseclick function txt = join(lines, '\n'); 
+  text(poem, 600, 400)
+  }
 
+  //define poem within mouseclick function txt = join(lines, '\n');
 }
