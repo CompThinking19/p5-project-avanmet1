90fd4a32a467cd6bfde7231c295ba7541d7cfa77
avanmet1 <55400228+avanmet1@users.noreply.github.com>
2019-12-10 17:51:41 -0500

    added background

--- sketch.js
+++ sketch.js
@@ -1,26 +1,31 @@
 
-var poem
+var bg
+var poem //saves variable 'poem' for later
+var stanza1
+var stanza2
 
 function preload(){
   dickinson = loadStrings('edcomplete.txt');
   poe = loadStrings('poecomplete.txt');
 }
+//reads in both txt files
 
 function setup() {
-  createCanvas(1200, 800);
-
+  bg = loadImage('backgroundtest3.jpg');
+  createCanvas(1332, 850);
   y = height;
 }
 
 function draw() {
   if (mouseIsPressed) {
-  var poem = random(poe) + '\n' + random(dickinson) + '\n' + random(poe) + '\n' +random(dickinson)
-  background(0);
+  var stanza1 = random(poe) + '\n' + random(dickinson) + '\n' + random(poe) + '\n' +random(dickinson) + '\n'
+  var stanza2 = random(poe) + '\n' + random(dickinson) + '\n' + random(poe) + '\n' +random(dickinson)
+  var poem = stanza1 + '\n' + stanza2
+  background(bg)
   fill(255);
   textSize(32);
   textAlign(CENTER);
-  text(poem, 600, 400)
+  text(poem, 650, 310);
   }
 
-  //define poem within mouseclick function txt = join(lines, '\n');
 }
